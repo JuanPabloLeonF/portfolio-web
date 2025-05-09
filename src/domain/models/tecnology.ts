fc71fea081bd5a5c210a032a6d8ee202f740a821
JuanPabloLeonF <129795557+JuanPabloLeonF@users.noreply.github.com>
2025-05-09 17:25:50 -0500

    Corrigiendo las urls de las imágenes

--- src/domain/models/tecnology.ts
+++ src/domain/models/tecnology.ts
@@ -8,76 +8,76 @@ export const tecnologies: TypeTecnology[] = [
     {
         name: 'Python',
         color: '#FFD700',
-        urlImg: '/images/python.svg'
+        urlImg: 'images/python.svg'
     },
     {
         name: 'HTML',
         color: '#E34F16',
-        urlImg: '/images/html.svg'
+        urlImg: 'images/html.svg'
     },
     {
         name: 'CSS',
         color: '#563D7C',
-        urlImg: '/images/css.svg'
+        urlImg: 'images/css.svg'
     },
     {
         name: 'JavaScript',
         color: '#F7DF1E',
-        urlImg: '/images/javascript.svg'
+        urlImg: 'images/javascript.svg'
     },
     {
         name: 'Angular',
         color: '#E05600',
-        urlImg: '/images/angular.svg'
+        urlImg: 'images/angular.svg'
     },
     {
         name: 'React',
         color: '#61DA96',
-        urlImg: '/images/react.svg'
+        urlImg: 'images/react.svg'
     },
     {
         name: 'Typescript',
         color: '#007ACC',
-        urlImg: '/images/typescript.svg'
+        urlImg: 'images/typescript.svg'
     },
     {
         name: 'Flask',
         color: '#19610a',
-        urlImg: '/images/flask.svg'
+        urlImg: 'images/flask.svg'
     },
     {
         name: 'Docker',
         color: '#2496ED',
-        urlImg: '/images/docker.svg'
+        urlImg: 'images/docker.svg'
     },
     {
         name: 'Git',
         color: '#F05032',
-        urlImg: '/images/git.svg'
+        urlImg: 'images/git.svg'
     },
     {
         name: 'GitHub',
         color: '#181717',
-        urlImg: '/images/github-svg.svg'
+        urlImg: 'images/github-svg.svg'
     },
     {
         name: 'MySQL',
         color: '#4479A1',
-        urlImg: '/images/mysql.svg'
+        urlImg: 'images/mysql.svg'
     },
     {
         name: 'MongoDB',
         color: '#47A248',
-        urlImg: '/images/mongo.svg'
+        urlImg: 'images/mongo.svg'
     },
     {
         name: 'Postgresql',
         color: '#336791',
-        urlImg: '/images/postgresql.svg'
+        urlImg: 'images/postgresql.svg'
     },
     {
         name: 'Kubernetes',
         color: '#326CE5',
-        urlImg: '/images/kubernetes.svg'
+        urlImg: 'images/kubernetes.svg'
     }
 ]
