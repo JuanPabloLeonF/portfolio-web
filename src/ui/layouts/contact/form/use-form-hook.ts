9256b62223c29fb00941847023717cb1cc8dd0cb
Juan Pablo Leon <fonseca123papo@gmail.com>
2025-05-09 16:21:28 -0500

    Corrigiendo errores de TypeScript para la CI/CD

--- src/ui/layouts/contact/form/use-form-hook.ts
+++ src/ui/layouts/contact/form/use-form-hook.ts
@@ -10,85 +10,80 @@ interface formSpanText {
 }
 
 export const useFormHook = () => {
-
     const { register, handleSubmit, reset, formState: { errors, isSubmitting }, trigger, watch } = useForm();
-      const [formSpanText, setFormSpanText] = useState<formSpanText>({
+    const [formSpanText] = useState<formSpanText>({ 
         name: "Nombre",
         email: "Correo",
         phone: "Teléfono",
         message: "Escríbeme",
-      });
-      const watchedName = watch("name");
-      const watchedEmail = watch("email");
-      const watchedPhone = watch("phone");
-      const watchedMessage = watch("message");
-    
-      const onSubmit = async (data: any) => {
+    });
+    const watchedName = watch("name");
+    const watchedEmail = watch("email");
+    const watchedPhone = watch("phone");
+    const watchedMessage = watch("message");
+
+    const onSubmit = async (data: any) => {
         try {
-          const response = await axios.post('https://formspree.io/f/xanekeny', data);
-    
-          if (response.status === 200) {
-            reset();
-            alert(`¡Gracias por escribirme! Te contactaré lo más pronto posible, ${data.name}.`);
-          } else {
-            alert('Hubo un error al enviar el formulario. error: ' + errors);
-          }
+            const response = await axios.post('https://formspree.io/f/xanekeny', data);
+
+            if (response.status === 200) {
+                reset();
+                alert(`¡Gracias por escribirme! Te contactaré lo más pronto posible, ${data.name}.`);
+            } else {
+                alert('Hubo un error al enviar el formulario. error: ' + JSON.stringify(errors)); // Muestra los errores en formato JSON para depuración
+            }
         } catch (error) {
-          alert('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
+            alert('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
         }
-      };
-    
-      const nameRules = {
+    };
+
+    const nameRules = {
         required: 'El nombre es requerido',
-        onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-          trigger('name');
-          setFormSpanText((prev) => ({ ...prev, name: errors?.name?.message || "Nombre" }));
+        onChange: () => {
+            trigger('name');
         },
         onBlur: () => trigger('name'),
-      };
-    
-      const emailRules = {
+    };
+
+    const emailRules = {
         required: 'El correo es requerido',
         pattern: {
-          value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-          message: 'El correo electrónico no es válido',
+            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+            message: 'El correo electrónico no es válido',
         },
-        onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-          trigger('email');
-          setFormSpanText((prev) => ({ ...prev, email: errors?.email?.message || "Correo" }));
+        onChange: () => {
+            trigger('email');
         },
         onBlur: () => trigger('email'),
-      };
-    
-      const phoneRules = {
+    };
+
+    const phoneRules = {
         required: 'El teléfono es requerido',
         pattern: {
-          value: /^[0-9]+$/,
-          message: 'El teléfono debe contener solo números',
+            value: /^[0-9]+$/,
+            message: 'El teléfono debe contener solo números',
         },
         maxLength: {
-          value: 100,
-          message: 'El teléfono debe tener al menos 7 dígitos',
+            value: 100,
+            message: 'El teléfono debe tener al menos 7 dígitos',
         },
-        onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-          trigger('phone');
-          setFormSpanText((prev) => ({ ...prev, phone: errors?.phone?.message || "Teléfono" }));
+        onChange: () => {
+            trigger('phone');
         },
         onBlur: () => trigger('phone'),
-      };
-    
-      const messageRules = {
+    };
+
+    const messageRules = {
         required: 'El mensaje es requerido',
         minLength: {
-          value: 10,
-          message: 'El mensaje debe tener al menos 10 caracteres',
+            value: 10,
+            message: 'El mensaje debe tener al menos 10 caracteres',
         },
-        onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-          trigger('message');
-          setFormSpanText((prev) => ({ ...prev, message: errors?.message?.message || "Escríbeme" }));
+        onChange: () => {
+            trigger('message');
         },
         onBlur: () => trigger('message'),
-      };
+    };
 
     return {
         formSpanText,
